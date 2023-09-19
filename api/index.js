77ae999084ab6da169dd01020b8e94f3e37301e9
Frank Onesso <odhiambofrank1965@gmail.com>
2023-09-19 17:08:28 +0300

    connected to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import  Express from "express";
+import Express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config(); //deotenv must be import and initialised
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongodb");
+  }) //if not
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = Express();
 
-app.listen(3000,()=>{
-    console.log("listening to port 3000!");
+app.listen(3000, () => {
+  console.log("listening to port 3000!");
 });
