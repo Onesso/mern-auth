3869d99ba3c704df471c9c0a37c297e1f2706789
Frank Onesso <odhiambofrank1965@gmail.com>
2023-09-19 16:05:42 +0300

    create and run the server using expres

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="bg bg-slate-200">
+    <div className="bg-slate-300">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold">Auth App</h1>
